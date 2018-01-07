477caecdb36821b431cd1bc8e9352cdf085ad9e8
pesanmerpati <rohmatsuseno@gmail.com>
2018-01-07 21:22:41 +0700

    Update version 1.0

--- app/src/main/java/com/wordpress/pesanmerpati/realtimechat/Activity/MainActivity.java
+++ app/src/main/java/com/wordpress/pesanmerpati/realtimechat/Activity/MainActivity.java
@@ -1,12 +1,14 @@
 package com.wordpress.pesanmerpati.realtimechat.Activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -19,6 +21,8 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -26,13 +30,21 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.wordpress.pesanmerpati.realtimechat.Adapter.MessageAdapter;
+import com.wordpress.pesanmerpati.realtimechat.BuildConfig;
 import com.wordpress.pesanmerpati.realtimechat.Pojo.ChatMessage;
 import com.wordpress.pesanmerpati.realtimechat.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +52,10 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String ANONYMOUS = "anonymous";
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+    public static final String REALTIMECHAT_MSG_LENGTH_KEY = "realtime_msg_length";
+
     public static final int RC_SIGN_IN = 1;
+    private static final int RC_PHOTO_PICKER = 2;
 
 
     private ListView mListViewMessage;
@@ -58,6 +73,10 @@ public class MainActivity extends AppCompatActivity {
     private ChildEventListener mChildEventListener;
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseStorage mFirebaseStorage;
+    private StorageReference mChatPhotoStorageReferences;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,8 +88,10 @@ public class MainActivity extends AppCompatActivity {
         //initial firebase commponent
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseStorage = FirebaseStorage.getInstance();
 
         mMessagesDatabaseReferences = mFirebaseDatabase.getReference().child("messages");
+        mChatPhotoStorageReferences = mFirebaseStorage.getReference().child("chat_photos");
 
         //initial reference to view
         mProgressBar = findViewById(R.id.progerssBar);
@@ -91,7 +112,10 @@ public class MainActivity extends AppCompatActivity {
         mImageButttonPicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
             }
         });
         // Enable Send button when there's text to send
@@ -115,9 +139,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        mEditTextMessage.setFilters(new InputFilter[] {
-                new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)
-        });
+        mEditTextMessage.setFilters(new InputFilter[] {new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
+
         mButtonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -156,6 +179,15 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                .build();
+        mFirebaseRemoteConfig.setConfigSettings(configSettings);
+
+        Map<String, Object> defaultConfigMap = new HashMap<>();
+        defaultConfigMap.put(REALTIMECHAT_MSG_LENGTH_KEY, DEFAULT_MSG_LENGTH_LIMIT);
+        mFirebaseRemoteConfig.setDefaults(defaultConfigMap);
+        fetchConfig();
     }
 
     @Override
@@ -168,6 +200,22 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Sign in canceled", Toast.LENGTH_LONG).show();
                 finish();
             }
+        } else if (requestCode == RC_PHOTO_PICKER && resultCode == RESULT_OK) {
+            Uri selectedImageUri = data.getData();
+
+            StorageReference photoRef = mChatPhotoStorageReferences.child(selectedImageUri.getLastPathSegment());
+
+            //upload photo to firebase storage
+            photoRef.putFile(selectedImageUri).addOnSuccessListener
+                    (this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    Uri photoUrl = taskSnapshot.getDownloadUrl();
+                                    ChatMessage chatMessage = new ChatMessage(null, mUsername, photoUrl.toString());
+                                    mMessagesDatabaseReferences.push().setValue(chatMessage);
+                                }
+                            }
+                    );
         }
     }
 
@@ -254,5 +302,47 @@ public class MainActivity extends AppCompatActivity {
             mMessagesDatabaseReferences.removeEventListener(mChildEventListener);
             mChildEventListener = null;
         }
+    }
+
+    private void fetchConfig() {
+        long cacheExpireation = 3600; //to specify the cache expiration time, set it to an hour for production builds.
+
+        //this allows when user debuggin app to get the latest values from firebase there are any changes immediately.
+        if (mFirebaseRemoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()) {
+            cacheExpireation = 0;
+        }
+        mFirebaseRemoteConfig.fetch(cacheExpireation)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Make the fetched config available
+                        // via FirebaseRemoteConfig get<type> calls, e.g., getLong, getString.
+                        mFirebaseRemoteConfig.activateFetched();
+
+                        // Update the EditText length limit with
+                        // the newly retrieved values from Remote Config.
+                        applyRetrievedlengthLimit();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error fetching config", e);
+
+                        // Update the EditText length limit with
+                        // the newly retrieved values from Remote Config.
+                        applyRetrievedlengthLimit();
+                    }
+                });
+    }
+
+    /**
+     +     * Apply retrieved length limit to edit text field. This result may be fresh from the server or it may be from
+     +     * cached values.
+     +     */
+    private void applyRetrievedlengthLimit() {
+        Long realtimechat_msg_length = mFirebaseRemoteConfig.getLong(REALTIMECHAT_MSG_LENGTH_KEY);
+        mEditTextMessage.setFilters(new InputFilter[]{new InputFilter.LengthFilter(realtimechat_msg_length.intValue())});
+        Log.d(TAG, REALTIMECHAT_MSG_LENGTH_KEY + " = " + realtimechat_msg_length);
     }
 }
